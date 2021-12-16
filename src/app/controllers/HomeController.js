ba4a5a364dc6634c1b96fc8116d8a4a36c599e77
ongimkshp <doducmanh.bb@gmail.com>
2021-12-16 21:56:25 +0700

    test-ăpi

--- src/app/controllers/HomeController.js
+++ src/app/controllers/HomeController.js
@@ -1,22 +1,17 @@
-const Users = require('../models/user')
 const Customers = require('../models/customer')
-const Subject = require('../models/subjects')
+const Subject =  require ('../models/subjects')
 
 class HomeController {
     show(req, res, next) {
-        Subject.findAll({
-            raw: true,
-        })
-        .then((users) => {
-            Subject.create({
-                subject_id: 'IT0101',
-                name: 'English For ICT',
-                credit: '5',
-                percentage_final_exam: '50'
+        Subject.findAll(
+       )
+        .then( users => {
+            res.json({
+                Message: 'Success',
+                data: users
             })
-            console.log(users)
-            res.json(users);
         })
+      
         .catch(next);
     }
     index(req, res, next) {
--- src/app/models/customer.js
+++ src/app/models/customer.js
@@ -3,9 +3,10 @@ const db = require('../../config/pg_db/index.js')
 
 const Customer = db.define('Customers',{
     CustomerID: {
-        type: sequelize.STRING,
+        type: sequelize.INTEGER,
         primaryKey: true,
-        
+        unique: true,
+        allowNull: false
     },
     Name:{
         type: sequelize.STRING
