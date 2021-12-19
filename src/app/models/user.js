9e17230199862c65854c31135df8f3be3a4eafe2
ongimkshp <doducmanh.bb@gmail.com>
2021-12-19 20:58:54 +0700

    add-model-user

--- src/app/models/user.js
+++ src/app/models/user.js
@@ -1,8 +1,37 @@
 const sequelize = require('sequelize')
 const db = require('../../config/pg_db/index.js')
 
-const User = db.define('Users')
-
+const User = db.define('user', {
+    userID: {
+        type: sequelize.STRING,
+        allowNull: false,
+        primaryKey: true,
+    },
+    firstName: {
+        type: sequelize.STRING
+    },
+    lastName: {
+        type: sequelize.STRING
+    },
+    email: {
+        type: sequelize.STRING
+    },
+    password: {
+        type: sequelize.STRING
+    },
+    MSSV: {
+        type: sequelize.STRING
+    },
+    SĐT: {
+        type: sequelize.STRING
+    },
+    address: {
+        type: sequelize.STRING
+    },
+    authorize: {
+        type: sequelize.STRING
+    },
+})
 
 module.exports = User
 
