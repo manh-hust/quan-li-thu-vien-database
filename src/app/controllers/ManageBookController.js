eb9332bdc406292a678b8840c41c56aa889882ef
ongimkshp <doducmanh.bb@gmail.com>
2022-01-06 16:56:44 +0700

    search-books

--- src/app/controllers/ManageBookController.js
+++ src/app/controllers/ManageBookController.js
@@ -19,20 +19,18 @@ class ManageBookController {
     async search(req, res, next){
         try {
             const {bookName, author, category} = req.body
-            if(bookName == '' && author =='' && category == ''){
+            if(category == '' && bookName == '' && author == '' ){
                 res.redirect('back')
                 return
             }
-  
-            const books = await Title.findAll({
-                where: {
-                    name: {
-                        [Op.like]: `%${bookName}%`
-                    }
-                }
-            })
+            console.log(bookName, author, category)
+            // and title.name ilike '%${bookName}%' and author.first_name ilike '%${author}%' 
+            const books = await Client.query(`select title.name as name, title.quantity, type.name as typename, author.first_name, author.last_name from title, author, type where title.author_id = author.author_id and title.type_id = type.type_id and title.type_id = '${category}'`)
             res.render('manage-books/index',{
-                books: books.rows
+                books: books.rows,
+                bookName,
+                author,
+                category
             })
         } catch (error) {
             res.send(error.message)            
